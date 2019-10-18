abce442404210126bd9882c7d625c08895e22be6
Vinodhini Chockalingam <chockali@arcesium.com>
2019-10-18 20:19:56 +0530

    Exploring Spring batch
    Starting with Spring boot for POC.

---
+++ src/test/java/com/foo/batch/HelloWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foo.batch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HelloWorldApplication.class)
+public class HelloWorldApplicationTests {
+
+    @Test
+    public void testContextLoads() {
+
+    }
+}
