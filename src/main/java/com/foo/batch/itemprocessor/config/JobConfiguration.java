62da723b6c5d3bd36e7efd53612693122cd55401
Vinodhini Chockalingam <chockali@arcesium.com>
2019-10-20 19:14:23 +0530

    Trying out Validating item processors

--- src/main/java/com/foo/batch/itemprocessor/config/JobConfiguration.java
+++ src/main/java/com/foo/batch/itemprocessor/config/JobConfiguration.java
@@ -15,6 +15,9 @@ import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.batch.item.file.transform.LineAggregator;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.batch.item.validator.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -84,6 +87,22 @@ public class JobConfiguration {
         }
     }
 
+    static class CustomerValidator implements Validator<Customer> {
+
+        @Override
+        public void validate(Customer value) throws ValidationException {
+            if (value.getFirstName().equalsIgnoreCase("dwight")) {
+                throw new ValidationException("I dont like Dwight");
+            }
+        }
+    }
+
+    @Bean
+    public ValidatingItemProcessor<Customer> customerValidatingItemProcessor() {
+        ValidatingItemProcessor<Customer> customerValidatingItemProcessor = new ValidatingItemProcessor<>(new CustomerValidator());
+        customerValidatingItemProcessor.setFilter(true); // if this is not set, any validation error will fail your entire job
+        return customerValidatingItemProcessor;
+    }
     @Bean
     public ItemProcessor<Customer,Customer> upperCaseItemProcessor() {
         return new UpperCaseItemProcessor();
@@ -101,7 +120,8 @@ public class JobConfiguration {
         return stepBuilderFactory.get("step1")
                 .<Customer, Customer>chunk(3)
                 .reader(customerFlatFileItemReader())
-                .processor(upperCaseItemProcessor())
+//                .processor(upperCaseItemProcessor())
+                .processor(customerValidatingItemProcessor())
                 .writer(customerItemWriter())
                 .build();
     }
